4e8b66a02ac875c19041aa777753236a5837d88f
iralitv <airyne.litvinchuk@gmail.com>
2022-09-07 10:08:57 +0300

    initial commit

--- storybook/stories/Button/index.js
+++ storybook/stories/Button/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import { TouchableHighlight } from 'react-native';
 
 export default function Button({ onPress, children }) {
@@ -8,10 +7,5 @@ export default function Button({ onPress, children }) {
 
 Button.defaultProps = {
   children: null,
-  onPress: () => {},
-};
-
-Button.propTypes = {
-  children: PropTypes.node,
-  onPress: PropTypes.func,
+  onPress: () => { },
 };
